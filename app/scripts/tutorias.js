92c7dd0496e176483948e3ae999614ea16e80e79
Didage <didage@gmail.com>
2015-04-13 13:09:32 -0500

    Repositorio para el proyecto de tutorías
    Sección del front end

---
+++ app/scripts/tutorias.js
@@ -0,0 +1,32 @@
+(function(){
+	var app = angular.module('tutorias', []);
+	
+	var currentSelection = 0;
+	
+	app.directive('pageHeader', function(){
+		return {
+			restrict: 'E',
+			templateUrl: '../pages/page-header.html',
+			controller: function(){
+				this.content = student;
+				this.logged = false;
+				this.login = function(){
+					this.logged = true
+				};
+				this.setCurrentSelection = function(num){
+					currentSelection = num;
+				};
+			},
+			controllerAs: 'log'
+		};
+	});
+	
+	
+	
+	
+	var exampleStudent = {
+		name: 'da.tovar10',
+		code: 201512531
+	}
+		
+})();
